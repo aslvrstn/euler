a0f1289bfba8bce81b0cff4c8cef0f05ebe40f2b
Alex Silverstein <aslvrstn@gmail.com>
2011-01-16 12:44:15 +0000

    Much less dumb isPalindrome for p4

--- p4/p4.hs
+++ p4/p4.hs
@@ -7,6 +7,4 @@ palindromes = (filter isPalindrome (concatMap (foo) [100..999]))
 foo n = map (*n) [n..999]
 
 isPalindrome n = let w = show n
-                     fstT = fst (splitAt (div (length w + 1) 2) w)
-                     sndT = snd (splitAt (div (length w) 2) w)
-                     in fstT == reverse sndT
+                     in w == reverse w
