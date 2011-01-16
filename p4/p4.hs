425a4427cdad42066819be073f3312c5efad6dca
Alex Silverstein <aslvrstn@gmail.com>
2011-01-16 12:27:10 +0000

    P4 done

--- p4/p4.hs
+++ p4/p4.hs
@@ -1,3 +1,7 @@
-main = print (length (concatMap (foo) [100..999]))
+main = print (filter isPalindrome (concatMap (foo) [100..999]))
 
 foo n = map (*n) [n..999]
+
+isPalindrome n = let w = show n
+                     tup = splitAt (div (length w) 2) w
+                     in fst tup == reverse (snd tup)
