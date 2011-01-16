665b8fae1eeabe1bfba192dfae9e10bb43d9ee1e
Alex Silverstein <aslvrstn@gmail.com>
2011-01-16 12:40:12 +0000

    Actual p4

--- p4/p4.hs
+++ p4/p4.hs
@@ -1,7 +1,12 @@
-main = print (filter isPalindrome (concatMap (foo) [100..999]))
+import Data.List
+
+main = print (sort palindromes)
+
+palindromes = (filter isPalindrome (concatMap (foo) [100..999]))
 
 foo n = map (*n) [n..999]
 
 isPalindrome n = let w = show n
-                     tup = splitAt (div (length w) 2) w
-                     in fst tup == reverse (snd tup)
+                     fstT = fst (splitAt (div (length w + 1) 2) w)
+                     sndT = snd (splitAt (div (length w) 2) w)
+                     in fstT == reverse sndT
