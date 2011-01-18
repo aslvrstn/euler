18b9b6c434c1bbf76cbba1d75e230542d1806cd0
Alex Silverstein <aslvrstn@gmail.com>
2011-01-18 00:50:33 +0000

    Add triangle generator

--- p12/p12.hs
+++ p12/p12.hs
@@ -1,3 +1,5 @@
-main = print $ filter ((>500).length) $ map divisors [1..]
+main = print $ filter ((>400).length) $ map divisors tris
+
+tris = 0 : (map (\n -> tris!!(n-1)+n) [1..])
 
 divisors n = filter ((==0).(mod n)) [1..n]
