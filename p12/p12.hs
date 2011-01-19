39babeb5c1e9723ad659dcede0af36c9018b176e
Alex Silverstein <aslvrstn@gmail.com>
2011-01-19 23:45:17 +0000

    Faster p12

--- p12/p12.hs
+++ p12/p12.hs
@@ -1,8 +1,10 @@
 import Euler
 import Data.List
 
-main = print $ head $ filter ((>500).length.divisors) $ tris
+--main = print $ head $ filter ((>500).length.divisors) $ tris
+main = print $ head $ filter ((>500).numDivisors) $ tris
 
 tris = 0 : (map (\n -> tris!!(fromIntegral(n)-1)+n) [1..])
 
-divisors n = nub $ map product $ subsequences $ primeFactors n
+--divisors n = nub $ map product $ subsequences $ primeFactors n
+numDivisors n = product $ map ((+1).length) $ group $ primeFactors n
