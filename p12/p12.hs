6e35d6bbe70373285aa451dde25b37da140c9a58
Alex Silverstein <aslvrstn@gmail.com>
2011-01-19 23:00:52 +0000

    Better divisors. Type issue

--- p12/p12.hs
+++ p12/p12.hs
@@ -1,5 +1,8 @@
-main = print $ filter ((>400).length) $ map divisors tris
+import Euler
+import Data.List
+
+--main = print $ filter ((>400).length) $ map divisors tris
 
 tris = 0 : (map (\n -> tris!!(n-1)+n) [1..])
 
-divisors n = filter ((==0).(mod n)) [1..n]
+divisors n = nub $ map product $ subsequences $ primeFactors n
