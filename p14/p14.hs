ac1777f22e25aecd789c9f99a28519fbd941ef84
Alex Silverstein <aslvrstn@gmail.com>
2011-01-20 20:45:06 +0000

    Working p14

--- p14/p14.hs
+++ p14/p14.hs
@@ -1,3 +1,8 @@
-main = print $ maximum $ take 1000000 chainLen
+import Data.List
 
-chainLen = [0,1] ++ map (\n -> if (even n) then chainLen!!(n `div` 2)+1 else chainLen!!(3*n+1)+1) [2..]
+main = print $ maximumBy (\a b -> compare (snd a) (snd b)) $ map (\n -> (n, length $ chain n)) [1..1000000]
+
+chain 1 = [1]
+chain n = if (even n) then n : chain (n `div` 2) else n : chain (3*n+1)
+
+--chainLen = [0,1] ++ map (\n -> if (even n) then chainLen!!(n `div` 2)+1 else chainLen!!(3*n+1)+1) [2..]
