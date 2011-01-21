26c4a9ea2db45c6e58d65b441ccf3fba3d82fd78
Alex Silverstein <aslvrstn@gmail.com>
2011-01-21 00:54:54 +0000

    p18

---
+++ p18/p18.hs
@@ -0,0 +1,13 @@
+main = do
+         f <- readFile "triangle.txt"
+         print $ maxPath $ triangle f
+
+maxPath [] = 0
+maxPath t@([root]:_) = root + max (maxPath $ lTriangle t) (maxPath $ rTriangle t)
+
+lTriangle ([root]:rows) = map (\l -> take ((length l)-1) l) rows
+
+rTriangle ([root]:rows) = map (drop 1) rows
+
+triangle :: String -> [[Integer]]
+triangle f = map (\x -> map read x) (map words $ lines f)
