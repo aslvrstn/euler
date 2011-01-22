8bc8258a6a731e2fcea8e3970043f26d4f563d7c
Alex Silverstein <aslvrstn@gmail.com>
2011-01-22 11:06:56 +0000

    p21

---
+++ p21/p21.hs
@@ -0,0 +1,9 @@
+import Euler
+import Data.List
+
+main = print $ sum $ filter amicable [2..10000]
+
+amicable a = let b = (sum $ init $ divisors a)
+             in a /= b && (sum $ init $ divisors b) == a
+
+divisors n = nub $ map product $ subsequences $ primeFactors n
