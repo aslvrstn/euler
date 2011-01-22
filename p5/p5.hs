62adda938c4cc32a8d79bbc50e382c1e2ca52642
Alex Silverstein <aslvrstn@gmail.com>
2011-01-22 17:08:31 +0000

    Change foldr to fold

--- p5/p5.hs
+++ p5/p5.hs
@@ -1,6 +1,6 @@
 import Data.List
 import Euler
 
-main = print $ product $ foldr (addMissingTo) [] $ map primeFactors [1..20]
+main = print $ product $ foldl (addMissingTo) [] $ map primeFactors [1..20]
 
 addMissingTo l1 l2 = concat [l1 \\ l2, l2]
