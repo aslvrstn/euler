bc798fa9e9b0e7b09dc6f85a3599fe0df31cc7b3
Alex Silverstein <aslvrstn@gmail.com>
2011-01-23 21:00:08 +0000

    Tighter bounds on p32

--- p32/p32.hs
+++ p32/p32.hs
@@ -3,7 +3,7 @@ import Maybe
 
 main = print $ sum $ nub $ concatMap pandig $ permutations "123456789"
 
-pandig l = mapMaybe (\p -> pandig' p) [(take x l, take y (drop x l), drop (x+y) l) | x <- [1..7], y <- [1..8-x]]
+pandig l = mapMaybe (\p -> pandig' p) [(take x l, take y (drop x l), drop (x+y) l) | x <- [1..4], y <- [1..4]]
            where pandig' (x,y,z) = let
                                      xr = read x
                                      yr = read y
