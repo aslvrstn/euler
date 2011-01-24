f621ec076f8777a4ce2a3098a5b1956bedac91a9
Alex Silverstein <aslvrstn@gmail.com>
2011-01-24 23:17:24 +0000

    p35

--- p35/p35.hs
+++ p35/p35.hs
@@ -1,3 +1,5 @@
-main = print $ 0
+import Euler
+
+main = print $ length $ filter (\n -> all (isPrime.read) $ rots $ show n) [1..999999]
 
 rots l = map (\x -> (drop x l ++ take x l)) [0..(length l)-1]
