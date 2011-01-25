97bf104084d951388bed6d6065d7e67916d41c73
Alex Silverstein <aslvrstn@gmail.com>
2011-01-25 22:07:07 +0000

    p36

---
+++ p36/p36.hs
@@ -0,0 +1,10 @@
+import Euler
+import Data.Bits
+import Data.List
+
+main = print $ sum $ filter (\n -> (isPalindrome $ digits n) && (isPalindrome $ toBinary n)) [1..999999]
+
+toBinary :: Int -> String
+toBinary n = dropWhile (=='0') $ map (\b -> if testBit n ((bitSize n)-b) then '1' else '0') [0..(bitSize n)]
+
+isPalindrome l = l == (reverse l)
