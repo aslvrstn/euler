6d52806373652632cd131030b065f5a21c16f467
Alex Silverstein <aslvrstn@gmail.com>
2011-01-25 23:58:01 +0000

    p39

--- p39/p39.hs
+++ p39/p39.hs
@@ -1,5 +1,7 @@
-main = print $ map (length.sols) [1..1000]
+import Data.List
 
-sols p = filter tri [(a,b,c) | a <- [1..p-2], b <- [a..p-a-1], let c = p-a-b]
+main = print $ maximumBy (\a b -> compare (snd a) (snd b)) $ zip [2,4..1000] $ map (length.sols) [2,4..1000]
+
+sols p = filter tri [(a,b,c) | a <- [1..div p 2], b <- [a..div (p-a) 2], let c = p-a-b]
 
 tri (a,b,c) = a^2+b^2==c^2
