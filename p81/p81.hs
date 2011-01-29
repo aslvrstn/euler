511d9ae75b12b34dca4b82ab1b35c59b6cfdcab1
Alex Silverstein <aslvrstn@gmail.com>
2011-01-29 21:35:46 +0000

    p81

---
+++ p81/p81.hs
@@ -0,0 +1,18 @@
+import Char
+
+main = do
+         f <- readFile "matrix.txt"
+         print $ parseFile f
+
+minPath f = minPath'
+            where 
+                  rows = fromIntegral $ length f
+                  cols = fromIntegral $ length (f!!0)
+                  minPath' = map minPathRow [0..rows-2] ++ [scanr1 (+) $ last f]
+                  minPathRow r = let ir = fromIntegral r
+                                 in map (\c -> f!!ir!!c + min (minPath'!!(ir+1)!!c) (minPath'!!ir!!(c+1))) [0..cols-2] ++ [f!!ir!!(cols-1) + minPath'!!(ir+1)!!(cols-1)]
+
+parseFile :: String -> [[Integer]]
+parseFile f = map parseLine $ lines f
+              where parseLine [] = []
+                    parseLine l = read (takeWhile isDigit l) : (parseLine $ dropWhile (not.isDigit) $ dropWhile (isDigit) l)
