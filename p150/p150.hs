b91311e35200d5899f39809608eafee12d9c88bd
Alex Silverstein <aslvrstn@gmail.com>
2011-01-29 11:16:57 +0000

    More work for triangle support in p150

--- p150/p150.hs
+++ p150/p150.hs
@@ -1,5 +1,17 @@
 main = print $ 0
 
-rng = 0 : map (\n -> t!!n - 2^19) [1..500500]
+-- Take out the elements from the sub-triangle starting at r and continuing for l levels
+subTri t r l = take (triNums!!l) $ drop r t
 
-t = 0 : map (\n -> (615949*t!!(n-1) + 797807) `mod` (2^20)) [1..500500]
+triNums = map (\n -> n*(n+1) `div` 2) [0..]
+
+rng = 0 : map (\n -> t!!n - 2^19) [1..500500]
+      where
+        t = 0 : map (\n -> (615949*t!!(n-1) + 797807) `mod` (2^20)) [1..500500]
+
+realTri = realTri' 1 $ drop 1 rng
+          where
+            realTri' _ [] = []
+            realTri' n t  = take n t : (realTri' (n+1) $ drop n t)
+
+exampleTri = [[15],[-14,-7],[20,-13,-5],[-3,8,23,-26],[1,-4,-5,-18,5],[-16,31,2,9,28,3]]
