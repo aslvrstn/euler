86a46b8a5eaeb16d49a9250da99d8403045c4740
Alex Silverstein <aslvrstn@gmail.com>
2011-07-17 15:32:09 +0100

    Still too slow

--- p62/p62.hs
+++ p62/p62.hs
@@ -1,12 +1,14 @@
 import Data.List
 import Data.Function
 
-main = print $ find (\c -> (length $ (filter isCube $ nub $ perms c)) == 5) cubes
+main = print $ find (\c -> (length $ (filter isCube $ geThan (perms c) c)) == 5) cubes
 
 cubes = map (flip (^) 3) [1..]
 
 isCube x = (round (fromIntegral x ** (1/3))) ^ 3 == x
 
-perms x = filter (\p -> (length $ show p) == lx) $ map read $ permutations sx
+geThan l x = filter (x<=) l
+
+perms x = nub $ filter (\p -> (length $ show p) == lx) $ map read $ permutations sx
     where sx = show x
           lx = length sx
