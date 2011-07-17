2fb4e44922cdb7bf89c94a7bdf5fc9ed5dfe8681
Alex Silverstein <aslvrstn@gmail.com>
2011-07-17 14:56:09 +0100

    [as] More work

--- p99/p99.hs
+++ p99/p99.hs
@@ -1,8 +1,15 @@
 main = do
          f <- readFile "base_exp.txt"
-         print $ pows f
+         print $ solve $ pows f
 
-pows :: String -> [(Integer, Integer)]
+solve pows = let maxLens = map maxLen $ pows
+             in filter ((len $ head pows)<=) maxLens
+
+len (b,e) = length $ show $ b^e
+
+maxLen (b,e) = let l = logBase 10 (fromIntegral b)
+               in (ceiling l)*e+1
+
 pows f = map (spanDrop ',') $ lines f
     where spanDrop c l = tailSnd $ span (c/=) l
           tailSnd (a,b) = (read a, read $ tail b)
