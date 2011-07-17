5e04836b7a3c8ade01c35807d8753bf14aa4ffcf
Alex Silverstein <aslvrstn@gmail.com>
2011-07-17 19:13:35 +0100

    [as] Works, except removeUsed needs to do quadrants

--- p96/p96.hs
+++ p96/p96.hs
@@ -1,31 +1,34 @@
 import Data.List.Split
 import Data.Char
 import Data.List
+import Data.Maybe
 
 main = do
          f <- readFile "sudoku.txt"
-         print $ parse f
+         print $ solve $ head $ parse f
 
---solve puzz = let singled = keepFillingSingles puzz
---             in if solved singled then (Just singled) else (if solvable singled then solve puzz else Nothing)
+solve puzz = solve' $ listForm puzz
 
---keepFillingSingles puzz = let onePass = fillSingles puzz
---                          in if (onePass == puzz) then puzz else keepFillingSingles onePass
+solve' puzz = let singled = keepRemovingUsed puzz
+              in if solved singled then (Just singled) else (if solvable singled then listToMaybe $ mapMaybe solve' $ collapseOne singled else Nothing)
+
+collapseOne (r:rs) = let collapsedRow = collapseOneR r
+                     in if null collapsedRow then map (r:) (collapseOne rs) else map (flip (:) rs) collapsedRow
+                     where collapseOneR [] = []
+                           collapseOneR (p:ps) = if length p == 1 then map (p:) (collapseOneR ps) else map (\pi -> [pi]:ps) p
+
+keepRemovingUsed puzz = let onePass = removeUsed puzz
+                        in if onePass == puzz then puzz else keepRemovingUsed onePass
 
 solvable puzz = all (all (\u -> (length u) /= 0)) puzz
 
 solved puzz = all (all (\u -> (length u) == 1)) puzz
 
-
 removeUsed puzz = transpose $ removeUsedRows $ transpose $ removeUsedRows puzz
     where 
-        removeUsedRows puzz = map (\r -> remove (singles r) r) puzz
-        singles r = concat $ filter (\p -> (length p == 1)) r
-        remove s ps = map (\p -> if length p == 1 then p else p \\ s) ps
-
---fillSingles puzz = map (map (\c@(x,y) -> let v = puzz!!x!!y in if v == 0 then (let uu = unused puzz c in if length uu == 1 then head uu else 0) else v)) coords
-
---coords = map (\x -> map (\y -> (x,y)) [0..8]) [0..8]
+        removeUsedRows puzz = map (\r -> map (remove (singlesRow r)) r) puzz
+        singlesRow r = concat $ filter (\p -> (length p == 1)) r
+        remove ss ps = if length ps == 1 then ps else ps \\ ss
 
 --unused puzz = map (map (unused' puzz)) coords
 --    where unused' puzz (x,y) = [1..9] \\ (puzz!!x ++ (transpose puzz)!!y ++ (sq puzz (x,y)))
