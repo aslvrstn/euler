49936aaef7a7edd630ef426662042f147c9e80c1
Alex Silverstein <aslvrstn@gmail.com>
2011-07-17 11:33:41 +0100

    [as] Clean up Euler

--- Euler.hs
+++ Euler.hs
@@ -1,20 +1,18 @@
-module Euler (fibs, primes, isPrime, primeFactors, divisors) where
+module Euler where
 
 import Data.List
 
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 primes = 2 : (filter (\n -> nothingDivides n $ takeWhile (\m -> m*m <= n) primes) [3,5..])
-
-nothingDivides n l = filter (\x -> mod n x == 0) l == []
+    where nothingDivides n l = filter (\x -> mod n x == 0) l == []
 
 isPrime n = (n > 1) && (length $ primeFactors n) == 1
 
 primeFactors 1 = [1]
-primeFactors n = factorHelper n primes
-
-factorHelper n (p:ps) | p*p > n         = [n]
-                      | n `mod` p == 0  = p : factorHelper (n `div` p) (p:ps)
-                      | otherwise       = factorHelper n ps
+primeFactors n = helper n primes
+    where helper n (p:ps) | p*p > n         = [n]
+                          | n `mod` p == 0  = p : helper (n `div` p) (p:ps)
+                          | otherwise       = helper n ps
 
 divisors n = nub $ map product $ subsequences $ primeFactors n
