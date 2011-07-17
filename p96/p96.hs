61009397080a4dc9f77cca7a276a0a3974889082
Alex Silverstein <aslvrstn@gmail.com>
2011-07-17 16:49:53 +0100

    Beginning of sudoku. Truly awful

---
+++ p96/p96.hs
@@ -0,0 +1,22 @@
+import Data.List.Split
+import Data.Char
+import Data.List
+
+main = do
+         f <- readFile "sudoku.txt"
+         print $ parse f
+
+keepFillingSingles puzz = let onePass = fillSingles puzz
+                          in if (onePass == puzz) then puzz else keepFillingSingles onePass
+
+solvable puzz = all (all (\c@(x,y) -> ((puzz!!x!!y /= 0) || (length $ unused puzz c) > 0))) coords
+
+fillSingles puzz = map (map (\c@(x,y) -> let v = puzz!!x!!y in if v == 0 then (let uu = unused puzz c in if length uu == 1 then head uu else 0) else v)) coords
+
+coords = map (\x -> map (\y -> (x,y)) [0..8]) [0..8]
+
+unused puzz (x,y) = [1..9] \\ (puzz!!x ++ (transpose puzz)!!y ++ (sq puzz (x,y)))
+    where sq puzz (x,y) = concatMap ((take 3) . (drop (3*(y `div` 3)))) $ take 3 $ drop (3*(x `div` 3)) puzz
+
+parse :: String -> [[[Int]]]
+parse f = map (map (map digitToInt)) $ map (drop 1) $ chunk 10 $ lines f
