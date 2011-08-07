20b82a914d28bf4b887d8a6bd8304ef9c9cf8ddc
Alex Silverstein <aslvrstn@gmail.com>
2011-08-07 17:05:46 +0100

    Done but slow

--- p73/p73.hs
+++ p73/p73.hs
@@ -1,6 +1,6 @@
 import Data.List
 import Data.Ratio
 
-maint = print $ sol
+main = print $ length $ sol 12000
 
-sol = takeWhile (< (1 % 2)) . dropWhile (<= (1 % 3)) . nub . sort $ concatMap (\n -> map (%n) [n `div` 3..n `div` 2]) [1..8]
+sol x = takeWhile (< (1 % 2)) . dropWhile (<= (1 % 3)) . nub . sort $ concatMap (\n -> map (%n) [n `div` 3..n `div` 2]) [1..x]
