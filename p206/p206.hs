3ed15158a37b2bb20efb7af4a342b4eaa0bd19c7
Alex Silverstein <aslvrstn@gmail.com>
2011-08-07 16:00:15 +0100

    Close

--- p206/p206.hs
+++ p206/p206.hs
@@ -1,10 +1,10 @@
 main = print $ 0
 
-filledIn :: [Integer]
-filledIn = map (read . intersp "1234567890") holes
+poss :: [Integer]
+poss = [read $ [a,b,c,d,e,f,g,h,i] | a <- ['1'], b <- ['0'..'3'], c <- ['0'..'9'], d <- ['0'..'9'], e <- ['0'..'9'], f <- ['0'..'9'], g <- ['0'..'9'], h <- ['3','7'], i <- ['0']]
 
-holes = [[a,b,c,d,e,f,g,h,i] | a <- ['0'..'9'], b <- ['0'..'9'], c <- ['0'..'9'], d <- ['0'..'9'], e <- ['0'..'9'], f <- ['0'..'9'], g <- ['0'..'9'], h <- ['3','7'], i <- ['0']]
+check n = "1234567890" == (eother $ show n)
 
-intersp xs [] = xs
-intersp [] ys = ys
-intersp (x:xs) y = x:(intersp y xs)
+eother [] = []
+eother [x] = [x]
+eother (x1:x2:xs) = x1:(eother xs)
