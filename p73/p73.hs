ef55d062ae5647735fe2e0bfa070e9227e84d695
Alex Silverstein <aslvrstn@gmail.com>
2011-08-07 17:20:46 +0100

    Smarter merge

--- p73/p73.hs
+++ p73/p73.hs
@@ -3,4 +3,11 @@ import Data.Ratio
 
 main = print $ length $ sol 12000
 
-sol x = takeWhile (< (1 % 2)) . dropWhile (<= (1 % 3)) . nub . sort $ concatMap (\n -> map (%n) [n `div` 3..n `div` 2]) [1..x]
+sol x = takeWhile (< (1 % 2)) . dropWhile (<= (1 % 3)) $ foldr1 mergeDD $ map (\n -> map (%n) [n `div` 3..n `div` 2]) [1..x]
+
+mergeDD a [] = a
+mergeDD [] b = b
+mergeDD as@(a:as') bs@(b:bs') = case (compare a b) of
+                                     LT -> a:(mergeDD as' bs)
+				     EQ -> a:(mergeDD as' bs')
+				     GT -> b:(mergeDD as bs')
